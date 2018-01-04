3ea0309c364642999dc5050da7bb3d1b2c18a540
Allen Frostline <allenfrostline@users.noreply.github.com>
2018-01-04 19:39:25 +0100

    Update run.py

--- forecast/run.py
+++ forecast/run.py
@@ -211,8 +211,8 @@ class CoinBot:
                 
 def main():
     coins = ['BTC', 'XRP', 'ETH', 'LTC', 'BCH', 'XMR', 'ZEC', 'DASH', 'ETC']
-    apikey = 'Y88KY4B1-ASMLC2C2-N0HO4J89-9I3PGNB4'
-    secret = 'e97e79187579648c9be3801b4e126601fac097e22e347f13963c130519c4b7a1ffb93872a1d493ac94c2dd0c7f84cc103bf06add17547fd60cce75842c8649eb'
+    apikey = ''
+    secret = ''
     t = CoinBot(coins, apikey, secret)
     t.start(False)
 
